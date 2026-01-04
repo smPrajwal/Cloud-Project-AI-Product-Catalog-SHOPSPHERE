7963db2f92d5deae9f3b5e0dc5add40e51f71d37
prajwal <prajwalprajwal1999@gmail.com>
2026-01-04 18:43:13 +0530

    Initializing and adding Terraform files to start working on it

---
+++ Azure_Terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.57.0, < 5.0.0"
+    }
+  }
+  cloud {
+    organization = "Practice_and_Project"
+    workspaces {
+      name = "Azure_Cloud_Project"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+locals {
+  default_rg = resource.azurerm_resource_group.main_rg.name
+}
+
+resource "azurerm_resource_group" "main_rg" {
+  name     = var.rg_name
+  location = var.default_loc
+  tags = {
+    Project = "ShopSphere"
+  }
+}
+
+resource "azurerm_storage_account" "main_sa" {
+  name = var.sa_name
+  resource_group_name = local.default_rg
+  location = var.default_loc
+  account_tier = var.sa_account_tier
+  account_replication_type = var.sa_replication_type
+  access_tier = var.sa_access_tier
+  allow_nested_items_to_be_public = var.sa_allow_public_access
+  tags = {
+    project     = "ShopSphere"
+    managed_by  = "terraform"
+  }
+}
---
+++ Azure_Terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "default_loc" {
+  description = "This represents the default location"
+  default = "South India"
+  type = string
+}
+
+variable "rg_name" {
+  description = "This represents the default resource group name"
+  default = "Project_ShopSphere"
+  type = string
+}
+
+variable "sa_name" {
+  description = "This represents the default storage account name"
+  default = "shopspheresa"
+  type = string
+}
+
+variable "sa_account_tier" {
+  description = "This represents the storage account tier"
+  default = "Standard"
+  type = string
+}
+
+variable "sa_replication_type" {
+  description = "This represents the storage account replication type"
+  default = "LRS"
+  type = string
+}
+
+variable "sa_access_tier" {
+  description = "This represents the storage account access tier"
+  default = "Hot"
+  type = string
+}
+
+variable "sa_allow_public_access" {
+  description = "This represents the storage account parameter to allow the nested items to be public or not"
+  default = false
+  type = bool
+}
