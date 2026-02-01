3bc6a6b9262d9d45b816accb7798dfbd3230bac9
prajwal <prajwalprajwal1999@gmail.com>
2026-02-01 23:17:31 +0530

    Removing unwanted lines of code

--- Azure_Terraform/modules/database/main.tf
+++ Azure_Terraform/modules/database/main.tf
@@ -46,10 +46,3 @@ resource "azurerm_private_endpoint" "db-endpoint" {
     private_dns_zone_ids = [azurerm_private_dns_zone.db-dns-zone.id]
   }
 }
-
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sql.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
